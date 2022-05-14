77ef6dea9760d22593648ce53988488cb053db92
Ethiel ADIASSA <ethiel97@gmail.com>
2022-05-14 01:01:44 +0000

    Update search.js
    Remote unused import

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import AnimeList from '../components/AnimeList'
 import { searchAnimes } from '../network/requests'
 import { useRouter } from 'next/router'
