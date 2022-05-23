174765cfd2f4699712d52e9f986de1ae3c588e69
Ethiel ADIASSA <ethiel97@gmail.com>
2022-05-23 23:01:56 +0000

    add seo tags

--- README.md
+++ README.md
@@ -1,34 +1,35 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Animeinfo
 
-## Getting Started
+A anime discovery, made with NextJs and TailwindCSS. Please deploy your own version of the site, by following the instructions. Please follow the instructions given below to deploy for yourself.
 
-First, run the development server:
+Jikan Api for fetching data which is totally free.
 
-```bash
-npm run dev
-# or
-yarn dev
+![Demo Image](https://github.com/Ethiel97/animeinfo/blob/master/assets/images/app_screenshot.jpg)
+
+
+## Deploy on your computer
+
+You need to have `nodejs` and `git` installed on your pc for following the intructions
+
+First download the repository using
+```
+git clone https://github.com/ethiel97/animeflix
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+This should download this repository to your computer. Next, to download the dependencies run
+```
+npm i
+```
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+Now build and start the production build of the site using
+```
+npm run build
+npm run start
+```
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+This will start the app on http://localhost:3000
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
 
-## Learn More
+## Contact
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Ethiel ADIASSA - [Twitter](https://www.twitter.com/enthusiastDev) - ethiel97@gmail.com - [Website](https://ethieladiassa.me)
--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import NextNProgress from 'nextjs-progressbar'
+import { NextSeo } from 'next-seo'
 import Header from './Header'
 import Head from 'next/head'
 export default function Layout({ children }) {
@@ -6,16 +7,51 @@ export default function Layout({ children }) {
     return (
         //define useful head tags for SEO using Head component from next
         <>
-            <Head>
-                <title>Anime Info</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <meta name="description" content="Anime Info" />
-                <meta name="keywords" content="Anime, List, Anime List" />
-                <meta name="author" content="Anime List" />
-                <meta name="robots" content="index, follow" />
-                <meta name="googlebot" content="index, follow" />
-                <meta name="google" content="notranslate" />
-            </Head>
+            <NextSeo
+                title="Animeinfo"
+                description="Browse, search thousands of anime for free. Get useful details about anime. Get anime recommendations. Get anime reviews. Get anime characters details."
+                canonical="https://animeinfo.vercel.app"
+                openGraph={{
+                    url: 'https://animeinfo.vercel.app',
+                    title: 'Open Graph Title',
+                    description: 'Open Graph Description',
+                    images: [
+                        {
+                            url: 'https://github.com/Ethiel97/animeinfo/blob/master/assets/images/app_screenshot.jpg',
+                            width: 1153,
+                            height: 1280,
+                            alt: 'Og Image Alt',
+                            type: 'image/jpg',
+                        },
+                    ],
+                    site_name: 'Animeinfo',
+                }}
+                twitter={{
+                    handle: '@enthusiastDev',
+                    site: '@enthusiastDev',
+                    cardType: 'summary_large_image',
+                }}
+                additionalLinkTags={[
+                    {
+                        rel: 'apple-touch-icon',
+                        href: '../public/apple-touch-icon.png',
+                        sizes: '128X128',
+                        type: "image/png"
+                    },
+                    {
+                        rel: 'icon',
+                        href: '../public/favicon-32X32.png',
+                        sizes: '32X32',
+                        type: "image/png"
+                    },
+                    {
+                        rel: 'icon',
+                        href: '../public/favicon-64X64.png',
+                        sizes: '64X64',
+                        type: "image/png"
+                    },
+                ]}
+            />
 
             <NextNProgress
                 color="#ef4444"
@@ -25,7 +61,6 @@ export default function Layout({ children }) {
 
             <Header />
             <main>{children}</main>
-
         </>
     )
 }
